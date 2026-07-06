99fd069219f49bab24aa1c538623f77067cbc9a7
vipin <swarnkarbipin73@gmail.com>
2026-07-06 10:53:16 +0530

    Complete Phase 1 - Express and MongoDB setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import connectDb from "./database/db.js"
+import connectDB from "./database/db.js";
+
+//load env variables 
+dotenv.config();
+
+//connect to mongodb
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+//start the express server 
 app.listen(PORT, () => {
   console.log(` Server is running on http://localhost:${PORT}`);
 });
